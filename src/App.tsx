58a8c65ca337fdc842c36362cbae7c8d7c46f687
Tanzida Nowshin <nowshinsara555@gmail.com>
2024-05-09 03:07:56 +0600

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Topbar from './scenes/global/topbar';
+import Sidebar from './scenes/global/Sidebar';
+import Dashboard from './scenes/dashboard';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Dashboard/>
+      <Topbar/>
+      <Sidebar/>
+      
     </div>
+    
   );
 }
 
